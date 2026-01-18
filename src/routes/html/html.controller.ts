09729fda11d6eed5cf34fb25b5835ae16e5c11fe
Alekyo4 <alexsandergomes4742@gmail.com>
2026-01-18 19:32:31 -0300

    feat: abort controller for requests

--- src/routes/html/html.controller.ts
+++ src/routes/html/html.controller.ts
@@ -4,10 +4,37 @@ import type { GetHtmlQuery } from "./html.schemas";
 
 export class HtmlController {
   public static async getHtml(request: FastifyRequest, reply: FastifyReply) {
-    const { url } = request.query as GetHtmlQuery;
+    const { url, timeoutMS } = request.query as GetHtmlQuery;
 
-    const html: string = await request.server.htmlService.getHtml(url, 60_000);
+    const controller: AbortController = new AbortController();
 
-    return reply.send({ html });
+    const timeout: NodeJS.Timeout = setTimeout(() => {
+      controller.abort();
+    }, timeoutMS);
+
+    request.raw.on("close", () => {
+      clearTimeout(timeout);
+
+      controller.abort();
+    });
+
+    try {
+      const html: string = await request.server.htmlService.getHtml(
+        url,
+        controller.signal,
+      );
+
+      return reply.send({
+        header: {
+          agent: "",
+          status: 200,
+        },
+        html: html,
+      });
+    } catch (err) {
+      if (!controller.signal.aborted) throw err;
+
+      return reply.code(499).send({ error: String(err) });
+    }
   }
 }
